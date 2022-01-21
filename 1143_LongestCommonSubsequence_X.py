9f2e3d5bb28c0d4bf76d8287f5ab4c7e83163548
the company <thecompany001@protonmail.com>
2022-01-21 11:01:48 -0500

    leet prac

---
+++ 1143_LongestCommonSubsequence_X.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def longestCommonSubsequence(self, text1, text2):
+        dp = [[0 for j in range(len(text2 + 1))] for i in range(len(text1) +1)]
+        
+        for i in range(len(text1) -1, -1, -1):
+            for j in range(len(text2) -1, -1, -1):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+        result dp[0][0]
