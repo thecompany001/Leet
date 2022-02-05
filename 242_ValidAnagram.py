7758bae0d060bdce9787322e023bd74a00b9d20c
the company <thecompany001@protonmail.com>
2022-02-05 18:27:08 -0500

    leet prac

--- 242_ValidAnagram.py
+++ 242_ValidAnagram.py
@@ -1,3 +1,15 @@
 class Solution(object):
     def isAnagram(self, s, t):
+        if len(S) != len(t):
+            return False
         
+        countS, countT = {}, {}
+        
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countS[t[i]] = 1 + countS.get(t[i], 0)   
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        
+        return True
