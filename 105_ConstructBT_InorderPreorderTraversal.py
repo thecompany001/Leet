ccda91032aa3bb73f1c9b26a7d6774ce12f249d1
the company <thecompany001@protonmail.com>
2022-02-06 14:28:38 -0500

    leet prac

--- 105_ConstructBT_InorderPreorderTraversal.py
+++ 105_ConstructBT_InorderPreorderTraversal.py
@@ -7,3 +7,4 @@ class Solution(object):
         mid = inorder.index(preorder[0])
         root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
         root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
