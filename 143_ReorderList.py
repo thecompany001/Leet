71ad64f0ed7ae77b0535d799cc4ff2df1720c9b1
the company <thecompany001@protonmail.com>
2022-02-06 20:33:39 -0500

    leet prac

--- 143_ReorderList.py
+++ 143_ReorderList.py
@@ -1,2 +1,24 @@
 class Solution(object):
     def reorderList(self, head):
+        slow, fast = head, head.next
+        while fast and fast.next:
+            slow = slow. next
+            fast = fast.next.next
+            
+        second = slow.next
+        prev = slow.next = None
+        
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+            
+        
+        # merge two halves
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
