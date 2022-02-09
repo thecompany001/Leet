c367b054a8731c35092fec7569bb18c276fa88e3
the company <thecompany001@protonmail.com>
2022-02-08 22:09:18 -0500

    leet prac

---
+++ 98_ValidBST.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isValidBST(self, root):
+        
+        def valid(node, left, right):
+            if not node:
+                return True
+            
+            if not (node.val < right and node.val > left):
+                return False
+            
+            return (valid(node.left, left, node.val) and
+            valud(node.right, node.val, right))
+        return valid(root, float("-inf"), float("inf"))
