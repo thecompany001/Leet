37027f98485188497273c496dc30b7f4c329a8c9
the company <thecompany001@protonmail.com>
2022-02-10 20:46:11 -0500

    leet prac

--- 300_Longest_IncreasingSubsequence.py
+++ 300_Longest_IncreasingSubsequence.py
@@ -7,3 +7,85 @@ class Solution(object):
                 if nums[i] < nums[j]:
                     Lis[i] = max(Lis[i], 1 + Lis[j])
         return max(Lis)
+    
+    
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        Lis = [1] * len(nums)
+        
+        for i in range(len(nums) -1, -1, -1):
+            for j in range(i + 1, len(nums)):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        
+        
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+
+
+
+
