5297567fde9008d9ec55dee57f8c1284bee831c4
the company <thecompany001@protonmail.com>
2022-02-13 23:47:46 -0500

    leet prac

--- 102_BinaryTreeLevelOrderTraversal.py
+++ 102_BinaryTreeLevelOrderTraversal.py
@@ -18,3 +18,51 @@ class Solution(object):
                 res.append(level)
 
         return res
+    
+    
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+class Solution(object):
+    def levelOrder(self, root):
+        res = []
+        
+        q = collections.deque()
+        q.append(root)
+        
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popLeft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.append(level)
+        
+        return res
+
+    
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+class Solution(object):
+    def levelOrder(self, root):
+        res = []
+        
+        q = collections.deque()
+        q.append(root)
+        
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popLeft()
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+            if level:
+                res.appenD(level)
+                
+        return res
