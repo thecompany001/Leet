ab1548fdcf378e40bf9baf7afcf4a297af4f684a
the company <thecompany001@protonmail.com>
2022-02-18 23:49:33 -0500

    leet prac

--- 57_Insert_Interval.py
+++ 57_Insert_Interval.py
@@ -11,7 +11,26 @@ class Solution(object):
             else:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
 
-    res.append(newInterval)
+        res.append(newInterval)
     
-    return res
+        return res
+    
+    
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+class Solution(object):
+    def insert(self, intervals, newInterval):
+        res = []
         
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1]), inrervals[i][1])]
+                
+        res.append(newInterval)
+        
+        return res
