c218bc021374b7038640f59bd0da4c3206206d69
the company <thecompany001@protonmail.com>
2022-02-18 23:50:03 -0500

    leet prac

--- 49_Group_Anagrams_X.py
+++ 49_Group_Anagrams_X.py
@@ -13,3 +13,23 @@ class Solution(object):
         return res.values()
     
     # O(m * n)
+    
+    
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    
+    
+class Solution(object):
+    def groupAnagrams(self, strs):
+        res = defaultdict(list) #mapping charCount to list of anagrams
+        
+        for s in strs:
+            count = [0] * 26 # a .... z
+            
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+                
+            res[tuple(count)].append(s)
+            
+        return res.values()
+    
+    # O(m * n)
