8a07bf0c6923fccf1fda04329e40e4f6aa18a62d
the company <thecompany001@protonmail.com>
2022-02-21 20:00:33 -0500

    leet prac

---
+++ 124_BinaryTree_MaximumPathSum_DFS.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxPathSum(self, root):
+        res = [root.val]
+        
+        def dfs(root):
+            if not root:
+                return 0
+            
+            leftMax = dfs(root.left)
+            rightMax = dfs(root.right)
+            leftMax = max(leftMax, 0)
+            rightMax = max(rightMax, 0)
+            
+            # compute max path sum WITH split
+            res[0] = max(res[0], root.val + leftMax + rightMax)
+            
+            return root.val + max(leftMax, rightMax)
+        
+        dfs(root)
+        return res[0]
