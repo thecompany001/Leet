b131f5e352b9822f67d8992ffc6a60e8d7422e6c
the company <thecompany001@protonmail.com>
2022-02-22 22:17:53 -0500

    leet prac

---
+++ 3_LongestSubstringNoRepeatChar_X.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        charSet = set()
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove([l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
