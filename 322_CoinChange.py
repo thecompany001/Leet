9e6cc184fde33387413044cbdcb9bc09d063eb4d
the company <thecompany001@protonmail.com>
2022-02-28 22:39:28 -0500

    leet prac

--- 322_CoinChange.py
+++ 322_CoinChange.py
@@ -254,32 +254,7 @@ class Solution(object):
                 if a - c >= 0:
                     dp[a] = min(dp[a], 1 + dp[a - c])
         return dp[amount] if dp[amount] != amount + 1 else -1
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
-
-class Solution(object):
-    def coinChange(self, coins, amount):
-        dp = [amount + 1] * [amount + 1]
-        dp[0] = 0
-        
-        for a in range(1, amount + 1):
-            for c in coins:
-                if a - c >= 0:
-                    dp[a] = min(dp[a], 1 + dp[a - c])
-        return dp[amount] if dp[amount] != amount + else -1
-    
-    
-    
-    
+     
     
     
     
@@ -300,11 +275,7 @@ class Solution(object):
     
     
     
-    
-    
-    
-
-    
+  
     
     
 XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
@@ -322,10 +293,21 @@ class Solution(object):
     
     
     
+
     
     
-    
-    
+XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        dp = [amount + 1] * [amount + 1]
+        dp[0] = 0
+        
+        for a in range(1, amount + 1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        return dp[amount] if dp[amount] != amount + else -1
     
     
     
