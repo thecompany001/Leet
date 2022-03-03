d9e0243399bb2cf68fff327f9688b094b78912be
the company <thecompany001@protonmail.com>
2022-03-02 21:52:01 -0500

    leet prac

--- 91_Decode_Ways.py
+++ 91_Decode_Ways.py
@@ -143,6 +143,18 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 class Solution:
     def numDecodings(self, s: str) -> int:
+        dp = { len(s) : 1 }
+        
+        for i in range(len(s) -1, -1, -1):
+            if s[i] = "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+            
+            if (i + 1 < len(s) and (s[i] == "1" or
+                s[i] == "2" and s[i + 1] in "0123456")):
+                dp[i] += dp[i + 2]
+        return dp[0]
         
         
         
@@ -151,6 +163,18 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 class Solution:
     def numDecodings(self, s: str) -> int:
+        dp = { len(s) : 1 }
+        
+        for i in range(len(s) -1, -1, -1):
+            if s[i] = "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+            
+            if (i + 1 < len(s) and (s[i] == "1" or
+                s[i] == "2" and s[i + 1] in "0123456")):
+                dp[i] += dp[i + 2]
+        return dp[0]
         
         
         
@@ -159,6 +183,18 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 class Solution:
     def numDecodings(self, s: str) -> int:
+        dp = { len(s) : 1 }
+        
+        for i in range(len(s) -1, -1, -1):
+            if s[i] = "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i + 1]
+            
+            if (i + 1 < len(s) and (s[i] == "1" or
+                s[i] == "2" and s[i + 1] in "0123456")):
+                dp[i] += dp[i + 2]
+        return dp[0]
         
         
         
