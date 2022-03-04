09254f2b8c1388fdc2ff173dbcb246ab05f68429
the company <thecompany001@protonmail.com>
2022-03-03 20:59:46 -0500

    leet prac

--- 206_Reverse_LinkedList_X.py
+++ 206_Reverse_LinkedList_X.py
@@ -31,6 +31,15 @@ XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 # Iterative
 class Solution(object):
+    def reverseList(self, head):
+        prev, cur = None, head
+        
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+        return prev
     
     
     
@@ -40,6 +49,19 @@ class Solution(object):
     
 # Recursive
 class Solution(object):
+    def reverseList(self, head):
+    
+    if not head:
+        return None
+    
+    newHead = head
+    if head.next:
+        newHead = self.reverseList(head.next)
+        head.next.next = head
+    head.next = None
+    
+    return newHead
+    
     
     
     
