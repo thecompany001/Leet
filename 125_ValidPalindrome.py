c4b63c368abfce78262fb6c1a18edaf94af06057
the company <thecompany001@protonmail.com>
2022-04-19 23:59:30 -0400

    leet prac

--- 125_ValidPalindrome.py
+++ 125_ValidPalindrome.py
@@ -1,2 +1,10 @@
 class Solution(object):
     def isPalindrome(self, s):
+        newStr = "":
+            
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[::-1]
+    
+def is
